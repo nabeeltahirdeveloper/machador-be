612ffb25094f55f3c3942bf9819918d930e30d79
Nabeel <nabeeltahirdeveloper@gmail.com>
2024-03-01 08:51:51 +0500

    Add redirect from HTTP to HTTPS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from application import app
 from datetime import datetime
 from flask import Flask, render_template, jsonify
 from flask_swagger import swagger
+from flask import request, redirect
 
 
 app=app
@@ -12,6 +13,14 @@ start_time = datetime.now()
 def main():
     elapsed_time = datetime.now() - start_time
     return render_template('index.html', version="1.0", time=elapsed_time)
+
+
+@app.before_request
+def before_request():
+    if request.headers.get('X-Forwarded-Proto') == 'http':
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 @app.route("/swagger.json")
